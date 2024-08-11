11f4b152d33eb6dca6551a234738202be217a2df
Abdelrady <ahmedmc95@gmail.com.com>
2024-08-11 12:51:20 +0300

    feat:side bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import "@/styles/globals.css"
 import { Metadata } from "next"
+import Head from "next/head"
 
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
+import { Sidebar } from "@/components/sidebar"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -31,24 +33,24 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-        >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
-              <div className="flex-1">{children}</div>
-            </div>
-            <TailwindIndicator />
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
+    <html lang="en" suppressHydrationWarning>
+      {/* <Head>
+      </Head> */}
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="relative flex min-h-screen flex-col">
+            {/* <SiteHeader /> */}
+            <Sidebar />
+            <main className="mx-5 mt-16 sm:ml-[300px] sm:mt-3">{children}</main>
+          </div>
+          {/* <TailwindIndicator /> */}
+        </ThemeProvider>
+      </body>
+    </html>
   )
 }
--- types/nav.ts
+++ types/nav.ts
@@ -1,6 +1,17 @@
+import { ReactNode } from "react"
+import { LucideIcon } from "lucide-react"
+
 export interface NavItem {
   title: string
   href?: string
   disabled?: boolean
   external?: boolean
+}
+export interface SidebarItems {
+  links: Array<{
+    label: string
+    href: string
+    icon?: LucideIcon
+  }>
+  extras?: ReactNode
 }
